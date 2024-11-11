6d0a055cb616b841b8eb28d22b64b36431d5e5c4
Mizakson <maxwellizakson@gmail.com>
2024-11-11 10:32:07 -0500

    Render message post form

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const app = express()
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs") 
+app.use(express.urlencoded({ extended: true }))
 
 const indexRouter = require("./routes/index")
-// const newRouter = require("./routes/new")
 
 app.get("/", indexRouter)
+app.get("/new", (req, res) => {
+    res.render("form")
+})
+
+app.post("/new", (req, res) => {
+    msg = req.body.msg
+    user = req.body.user
+    messages.push({ text: msg, user: user, added: new Date(), })
+    res.redirect("/")
+})
 
 const PORT = 3000
 app.listen(PORT, () => {
