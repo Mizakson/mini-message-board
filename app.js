71f17f806a13c57af3476757de1527a78f2ae1a7
Mizakson <maxwellizakson@gmail.com>
2024-11-19 10:16:09 -0500

    Cluster commit

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require("express")
 const path = require("node:path")
 const app = express()
+const { Router } = require("express")
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs") 
 app.use(express.urlencoded({ extended: true }))
 
-const indexRouter = require("./routes/index")
+const router = Router()
 
-app.get("/", indexRouter)
+const messages = [
+    {
+        text: "Hi...",
+        user: "Marie",
+        added: new Date(),
+    },
+    {
+        text: "I am evil...",
+        user: "Hal",
+        added: new Date(),
+    },
+    {
+        text: "I am good...",
+        user: "Clark",
+        added: new Date(),
+    }
+]
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Mini Messageboard", messages: messages })
+})
 app.get("/new", (req, res) => {
     res.render("form")
 })
