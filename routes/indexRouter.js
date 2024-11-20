c48f41a8de3c0b0da4f8d22d8b91c5717af99417
Mizakson <maxwellizakson@gmail.com>
2024-11-20 13:21:48 -0500

    Create messages array

---
+++ routes/indexRouter.js
@@ -0,0 +1,15 @@
+const { Router } = require("express")
+const indexRouter = Router()
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+]
