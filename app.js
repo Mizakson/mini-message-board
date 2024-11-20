fe7fb663ff0b5572fc31379ea614ee5af11644f5
Mizakson <maxwellizakson@gmail.com>
2024-11-20 13:17:46 -0500

    Add urlencoded, ejs view statements

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const app = express()
 
+
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+
 app.get("/", (req, res) => {
     res.send("...")
 })
